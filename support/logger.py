ae1450741db7d6815b6db02b84999c0057783d39
Nikita <5195769+nikkrichko@users.noreply.github.com>
2024-02-06 07:17:51 -0800

    Update logger and change main model in config
    In the logger module, we've now allowed more informative logging by providing execution time for function calls and detailing the name of the function and its respective arguments when run in debug level. We have also updated the 'MAIN_MODEL' in config.yaml from "gpt-4-0125-preview" to "gpt-3.5-turbo-0125".

--- support/logger.py
+++ support/logger.py
@@ -1,12 +1,13 @@
 import logging
 import support.Configurator as Config
+import time
 
 class Logger:
     def __init__(self):
         config = Config.Config()
-        log_level = config.get('LOG_LEVEL')
+        self.log_level = config.get('LOG_LEVEL')
         self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(log_level)
+        self.logger.setLevel(self.log_level)
 
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         handler = logging.StreamHandler()
@@ -14,18 +15,29 @@ class Logger:
         self.logger.addHandler(handler)
 
     def log(self, message):
-        if self.logger.level == logging.DEBUG:
+        if self.log_level == "DEBUG":
             self.logger.debug(message)
         else:
             self.logger.info(message)
 
 def delog():
     logger = Logger()
+
     def decorator(func):
         def wrapper(*args, **kwargs):
-            logger.log(f'Calling function: {func.__name__}')
+            start_time = time.time()
+
+            logger.log(f'{func.__name__} - started')
+
             result = func(*args, **kwargs)
-            logger.log(f'Function {func.__name__} finished')
+
+            end_time = time.time()
+            logger.log(f'{func.__name__} - finished in {end_time - start_time:.2f} seconds')
+
+            if logger.log_level == "DEBUG":
+                debug_message = f'Execution time for {func.__name__}: {end_time - start_time:.2f} seconds -- {func.__name__} - args: {args}, kwargs: {kwargs}'
+                logger.log(debug_message)
+
             return result
         return wrapper
     return decorator
